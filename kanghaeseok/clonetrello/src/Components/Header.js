65e048b9c9214f6ecc968f8f11c8084eea80cba8
HaeSeok Kang <hotbloodturtle@gmail.com>
2020-08-16 14:03:15 +0900

    header style 작업 진행 중
    n/a

--- kanghaeseok/clonetrello/src/Components/Header.js
+++ kanghaeseok/clonetrello/src/Components/Header.js
@@ -1,8 +1,18 @@
 import React from "react";
+import "./Header.css";
 
 const Header = () => {
     return (
-        <h1>난 헤더야</h1>
+        <div className="header_wrap">
+            <div className="left_container">
+                <div className="left__menu left__item"></div>
+                <div className="left__home left__item"></div>
+                <div className="left__boards_btn left__item"></div>
+                <div className="left__search left__item"></div>
+            </div>
+            <div className="center_container"></div>
+            <div className="right_container"></div>
+        </div>
     )
 }
 
