6b4740266b32eb0d8d3a631372b34d8394b7a8de
HaeSeok Kang <hotbloodturtle@gmail.com>
2020-08-30 11:11:30 +0900

    detail header left container
    n/a

--- kanghaeseok/clonetrello/src/Components/Header.js
+++ kanghaeseok/clonetrello/src/Components/Header.js
@@ -1,14 +1,27 @@
 import React from "react";
 import "./Header.scss";
+import { faTh, faHome, faChartBar, faSearch } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const Header = () => {
     return (
+        
         <div className="header_wrap">
             <div className="left_container">
-                <div className="left__menu left__item"></div>
-                <div className="left__home left__item"></div>
-                <div className="left__boards_btn left__item"></div>
-                <div className="left__search left__item"></div>
+                <div className="left__menu left__item">
+                    <FontAwesomeIcon icon={faTh} className="item__icon" />
+                </div>
+                <div className="left__home left__item">
+                    <FontAwesomeIcon icon={faHome} className="item__icon" />
+                </div>
+                <div className="left__boards_btn left__item">
+                    <FontAwesomeIcon icon={faChartBar} className="item__icon" />
+                    <span className="item__icon item__text">Boards</span>
+                </div>
+                <div className="left__search left__item">
+                    <input type="search" name="" id="input-search"/>
+                    <FontAwesomeIcon icon={faSearch} className="item__icon search-icon" />
+                </div>
             </div>
             <div className="center_container"></div>
             <div className="right_container"></div>
