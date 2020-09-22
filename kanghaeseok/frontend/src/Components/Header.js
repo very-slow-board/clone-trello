bc4bb409ea54a559fa5a17f79f4a68ace5ab9dbd
HaeSeok Kang <hotbloodturtle@gmail.com>
2020-09-22 10:16:52 +0900

    add: header-container layout

--- kanghaeseok/frontend/src/Components/Header.js
+++ kanghaeseok/frontend/src/Components/Header.js
@@ -2,8 +2,35 @@ import React from "react";
 import { Link, withRouter } from "react-router-dom";
 import styled from "styled-components";
 
-const Header = styled.div``;
+const Header = styled.div`
+  width: 100%;
+  height: 40px;
+  background-color: #00609a;
+  background-image: url("https://a.trellocdn.com/prgb/dist/images/header-logo-2x.01ef898811a879595cea.png");
+  background-repeat: no-repeat;
+  background-position: 50% 50%;
+  background-size: 80px 30px;
+  display: flex;
+  justify-content: space-between;
+`;
+
+const LeftContainer = styled.div`
+  width: 400px;
+  height: inherit;
+  background-color: red;
+`;
+
+const RightContainer = styled.div`
+  width: 200px;
+  height: inherit;
+  background-color: blue;
+`;
 
 const SLink = styled(Link)``;
 
-export default withRouter(() => <Header>Hi I'am Header</Header>);
+export default withRouter(() => (
+  <Header>
+    <LeftContainer />
+    <RightContainer />
+  </Header>
+));
--- kanghaeseok/frontend/src/Routes/Detail/DetailPresenter.js
+++ kanghaeseok/frontend/src/Routes/Detail/DetailPresenter.js
@@ -2,9 +2,6 @@ import React from "react";
 import styled from "styled-components";
 
 const Container = styled.div`
-  width: 100px;
-  height: 100px;
-  background-color: red;
 `;
 
 const DetailPresenter = () => <Container>detail</Container>;
